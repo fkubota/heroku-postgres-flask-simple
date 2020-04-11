a846e7f8bad5f5108f33fd64555dc7ced2a24b29
fkubota <rkkubotay@gmail.com>
2020-04-11 14:30:36 +0900

    add sqlalchemy

--- app.py
+++ app.py
@@ -2,8 +2,8 @@
 from flask import Flask
 from flask_restful import Resource, Api, reqparse
 from flask_cors import CORS
-import psycopg2
 import pandas as pd
+from sqlalchemy import create_engine
 
 app = Flask(__name__)
 CORS(app)
@@ -16,27 +16,28 @@ parser.add_argument("arg01")
 # データベースの接続情報
 connection_config = {
     'user': 'xxgvdlopjhsvsk',
-    'password': '38078c4a27e141748248c1c42b0ec966549d95df7a51537eb55e96a426fd035b',
+    'password': '38078c4a27e141748248c1c42b0ec96654 \
+                 9d95df7a51537eb55e96a426fd035b',
     'host': 'ec2-54-147-209-121.compute-1.amazonaws.com',
-    'port': '5432', # なくてもOK
+    'port': '5432',
     'database': 'd2fj6i9l6jq4tu'
 }
 
 
 class MyApi(Resource):
     def post(self):
-        print('func post')
-        # hello
-        print('- parse')
         args = parser.parse_args()
         val = args['arg01']
         val_val = val + ' ---> flask'
 
         # postgres
-        print('- postgres')
-        connection = psycopg2.connect(**connection_config)
-        df = pd.read_sql(sql='SELECT * FROM hello_table;', con=connection)
-        val_val = val_val + " --- df_colname: \n" + str(df)
+        engine = create_engine('postgresql://{user}: \
+                                {password}@{host}: \
+                                {port}/{database}'.format(**connection_config))
+
+        # read sql
+        df = pd.read_sql(sql='SELECT * FROM hello_table;', con=engine)
+        val_val = val_val + " --- df_colname: " + str(df)
 
         return {"after_api": val_val}
 
