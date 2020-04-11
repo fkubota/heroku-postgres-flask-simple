3a2a670710a9e021ec66f0d52c37634befe4da4b
fkubota <rkkubotay@gmail.com>
2020-04-11 11:49:23 +0900

    update

--- app.py
+++ app.py
@@ -2,6 +2,8 @@
 from flask import Flask
 from flask_restful import Resource, Api, reqparse
 from flask_cors import CORS
+import psycopg2
+import pandas as pd
 
 app = Flask(__name__)
 CORS(app)
@@ -11,12 +13,28 @@ api = Api(app)
 parser = reqparse.RequestParser()
 parser.add_argument("arg01")
 
+# データベースの接続情報
+connection_config = {
+    'user': 'xxgvdlopjhsvsk',
+    'password': '38078c4a27e141748248c1c42b0ec966549d95df7a51537eb55e96a426fd035b',
+    # 'host': 'ec2-54-147-209-121.compute-1.amazonaws.com',
+    # 'port': 'ポート番号', # なくてもOK
+    'database': 'd2fj6i9l6jq4tu'
+}
+
 
 class MyApi(Resource):
     def post(self):
+        # hello
         args = parser.parse_args()
         val = args['arg01']
         val_val = val + ' ---> flask'
+
+        # postgres
+        connection = psycopg2.connect(**connection_config)
+        df = pd.read_sql(sql='SELECT * FROM hello_table;', con=connection)
+        val_val = val_val + " --- df_colname: \n" + str(df)
+
         return {"after_api": val_val}
 
 
